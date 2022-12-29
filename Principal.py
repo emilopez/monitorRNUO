4c319d37e296bacff0b7a88c05b06eb4a93ec7d2
Emiliano <emiliano.lopez@gmail.com>
2022-12-29 14:39:27 -0300

    beta version

---
+++ Principal.py
@@ -0,0 +1,9 @@
+# https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+import streamlit as st
+
+st.set_page_config(
+    page_title="Monitor Reserva Natural Urbana del Oeste - Santa Fe",
+    page_icon="🍻",
+)
+
+st.write("# Monitor RNUO 🍻")
