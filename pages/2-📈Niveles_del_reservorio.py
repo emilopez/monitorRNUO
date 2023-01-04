979a44fa3afccee4d71b406533ac1d8c1114e807
Emiliano <emiliano.lopez@gmail.com>
2023-01-04 18:26:39 -0300

    distancia desde pasarela 1

--- pages/2-📈Niveles_del_reservorio.py
+++ pages/2-📈Niveles_del_reservorio.py
@@ -13,24 +13,43 @@ st.set_page_config(page_title="Niveles del Reservorio",
                     page_icon="📈", 
                     layout="wide")
 
-st.markdown("# Altura hidrométrica")
-st.write("""Altura canal pasarela 1""")
+st.markdown("# Niveles del Reservorio")
+st.write("""Altura hidrométrica en diferentes puntos del reservorio""")
 
 tab1, tab2 = st.tabs(["DATOS", "DISPOSITIVOS"])
+
+cwd = Path.cwd()
+fn  = cwd / "datos" / "distancia_R1.csv"
+r1_df = pd.read_csv(fn, sep=";")
+
+fechaR1 = r1_df["datetime"]
+distaR1 = r1_df["distancia"]
+voltaR1 = r1_df["voltaje"]
 
 with tab1:
     placeholder = st.empty()
 
     with placeholder.container():
 
-        xdata = np.arange(0,1,0.01)
-        ydata = np.sin(2*np.pi*xdata)
+        fig = go.Figure()
+        fig.add_trace(go.Scattergl(x = fechaR1, y = distaR1, mode="markers+lines", name="Canal 1"))
+        fig.add_trace(go.Scattergl(x = fechaR1, y = distaR1+50, mode="markers+lines", name="Alcantarilla 1 (falso)"))
+        fig.add_trace(go.Scattergl(x = fechaR1, y = distaR1+60, mode="markers+lines", name="Alcantarilla 2 (falso)"))
+        fig.add_trace(go.Scattergl(x = fechaR1, y = distaR1+70, mode="markers+lines", name="Reservorio (falso)"))
+
+
+        fig.update_layout(title="Niveles del reservorio (Distancia!)", barmode='group',legend=dict(orientation="h", yanchor="bottom", y=1.02,xanchor="right", x=0.9))
+
+        st.plotly_chart(fig, use_container_width=True)
+
+with tab2:
+    placeholder2 = st.empty()
+    with placeholder2.container():
 
         fig = go.Figure()
-        fig.add_trace(go.Scattergl(x = xdata, y = ydata + 1.5, mode="markers+lines", name="Canal 1"))
-        fig.add_trace(go.Scattergl(x = xdata, y = 1.5*ydata + 1.5, mode="markers+lines", name="Canal 2"))
-        fig.add_trace(go.Scattergl(x = xdata, y = ydata-0.2 + 1.5, mode="markers+lines", name="Reservorio"))
+        fig.add_trace(go.Scattergl(x = fechaR1, y = voltaR1, mode="markers+lines", name="Equipo R1"))
+        
 
-        fig.update_layout(title="Niveles del reservorio", barmode='group',legend=dict(orientation="h", yanchor="bottom", y=1.02,xanchor="right", x=0.9))
+        fig.update_layout(title="Voltaje equipos", barmode='group',legend=dict(orientation="h", yanchor="bottom", y=1.02,xanchor="right", x=0.9))
 
         st.plotly_chart(fig, use_container_width=True)
