7e92424a97fc2ed7266f0ab887c4f26caa8ce566
emilopez <emiliano.lopez@gmail.com>
2023-02-13 13:22:05 -0300

    fix date format

--- Principal.py
+++ Principal.py
@@ -103,7 +103,8 @@ with placeholder.container():
         cwd = Path.cwd()
         fn  = cwd / "datos" / "lluvia_cim" / "precipitacion.csv"
         
-        data_cim = pd.read_csv(fn, sep="\t", parse_dates=["Fecha"])
+        data_cim = pd.read_csv(fn, sep="\t", parse_dates=["Fecha"], dayfirst=True)
+        st.write(data_cim)
         xdata = data_cim["Fecha"]
         ydata = data_cim["Lectura"]
 
